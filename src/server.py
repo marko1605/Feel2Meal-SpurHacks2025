04569306a3b620318cbdb097acff2f214f925249
Storage <121151940+ShivamGlotra@users.noreply.github.com>
2025-06-22 03:59:26 -0400

    test

--- src/server.py
+++ src/server.py
@@ -9,4 +9,5 @@ app = Flask(__name__)
 @app.route('/backend', methods = ['POST'])
 def foo():
     print(request.get_json())
+    return {"myResponse":"Hello"}
     return {"myResponse": getNearbyPlaces()}
